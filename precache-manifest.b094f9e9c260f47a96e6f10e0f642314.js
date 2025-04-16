09e4bc43238e8bee0026eff945f2aa67e698f24a
Achal <achalbharadwaj018@gmail.com>
2025-04-16 22:40:05 +0530

    Updates

---
+++ precache-manifest.b094f9e9c260f47a96e6f10e0f642314.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600391c331e6b01614953199eaf2e07d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f485e613ee4495412cc0",
+    "url": "/static/css/2.7f5860a0.chunk.css"
+  },
+  {
+    "revision": "c0ddf965bfc88c0772f9",
+    "url": "/static/css/main.49d7d56d.chunk.css"
+  },
+  {
+    "revision": "f485e613ee4495412cc0",
+    "url": "/static/js/2.9695b8f9.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.9695b8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ddf965bfc88c0772f9",
+    "url": "/static/js/main.1e78057e.chunk.js"
+  },
+  {
+    "revision": "6de1138e117e4c0ae1f3",
+    "url": "/static/js/runtime-main.4c9dfae0.js"
+  },
+  {
+    "revision": "7ff1bae7ba837fd05fd3329959e79087",
+    "url": "/static/media/project1.7ff1bae7.jpg"
+  },
+  {
+    "revision": "142ff9d47a74dd4f95960b06e6c6637e",
+    "url": "/static/media/project2.142ff9d4.jpg"
+  },
+  {
+    "revision": "994e6a4a637403f22da5af6d09f1fbbe",
+    "url": "/static/media/project3.994e6a4a.jpg"
+  },
+  {
+    "revision": "c8cbb884eb845de5ad0ac2a2b9730c67",
+    "url": "/static/media/project4.c8cbb884.jpg"
+  },
+  {
+    "revision": "3865efd9bc6562269802c7816c6f2cbc",
+    "url": "/static/media/project5.3865efd9.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
