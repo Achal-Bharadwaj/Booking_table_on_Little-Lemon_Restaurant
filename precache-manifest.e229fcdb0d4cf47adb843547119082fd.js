8ebce1387473a0e28b94b0967179b3b53a54c38f
Achal <achalbharadwaj018@gmail.com>
2025-04-16 22:47:58 +0530

    Updates

---
+++ precache-manifest.e229fcdb0d4cf47adb843547119082fd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1670e7c933a8d4d52d5be083e296cf9",
+    "url": "/Achal-Portfolio/index.html"
+  },
+  {
+    "revision": "3f1fd5f47f2a4e1f6c5c",
+    "url": "/Achal-Portfolio/static/css/2.6e78ae67.chunk.css"
+  },
+  {
+    "revision": "c0ddf965bfc88c0772f9",
+    "url": "/Achal-Portfolio/static/css/main.49d7d56d.chunk.css"
+  },
+  {
+    "revision": "3f1fd5f47f2a4e1f6c5c",
+    "url": "/Achal-Portfolio/static/js/2.9695b8f9.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Achal-Portfolio/static/js/2.9695b8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ddf965bfc88c0772f9",
+    "url": "/Achal-Portfolio/static/js/main.1e78057e.chunk.js"
+  },
+  {
+    "revision": "d250a438dd2783379187",
+    "url": "/Achal-Portfolio/static/js/runtime-main.4b728fbd.js"
+  },
+  {
+    "revision": "7ff1bae7ba837fd05fd3329959e79087",
+    "url": "/Achal-Portfolio/static/media/project1.7ff1bae7.jpg"
+  },
+  {
+    "revision": "142ff9d47a74dd4f95960b06e6c6637e",
+    "url": "/Achal-Portfolio/static/media/project2.142ff9d4.jpg"
+  },
+  {
+    "revision": "994e6a4a637403f22da5af6d09f1fbbe",
+    "url": "/Achal-Portfolio/static/media/project3.994e6a4a.jpg"
+  },
+  {
+    "revision": "c8cbb884eb845de5ad0ac2a2b9730c67",
+    "url": "/Achal-Portfolio/static/media/project4.c8cbb884.jpg"
+  },
+  {
+    "revision": "3865efd9bc6562269802c7816c6f2cbc",
+    "url": "/Achal-Portfolio/static/media/project5.3865efd9.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Achal-Portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Achal-Portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Achal-Portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Achal-Portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b094f9e9c260f47a96e6f10e0f642314.js"
+  "/Achal-Portfolio/precache-manifest.e229fcdb0d4cf47adb843547119082fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Achal-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
